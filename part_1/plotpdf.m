b11599b9b3859b772568a597c18601ad49fdf3b7
akjanik <adam.k.janik@gmail.com>
2016-01-24 18:22:28 +0100

    Changed directory structure to simpler

--- part_1/plotpdf.m
+++
@@ -1,5 +0,0 @@
-xRange = 0:10;                %# Range of integers to compute a probability for
-N = hist(gaussian,xRange);        %# Bin the data
-plot(xRange,N./numel(gaussian));  %# Plot the probabilities for each integer
-xlabel('Integer value');
-ylabel('Probability');
